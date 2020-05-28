66740dca2ff5366300a192c2d7fc5c494a76424d
johns <jsantore@bridgew.edu>
2020-05-28 17:38:21 -0400

    fix flake8 error in test file

--- GetRecipeData.py
+++ GetRecipeData.py
@@ -1,5 +1,5 @@
 import requests
-
+import sqlite3
 
 def display_data(to_display):
     for recipe in to_display:
@@ -14,6 +14,20 @@ def get_data(location):
     return data['results']
 
 
+def save_data(recipes:list, cursor:sqlite3.Cursor):
+    for recipe in recipes:
+        cursor.execute("INSERT INTO recipes(title, web_address, ingredients) VALUES (?,?,?);",
+                       (recipe['title'], recipe['href'], recipe['ingredients']))
+
+
+def setup_database(cursor:sqlite3.Cursor):
+    create_statement = """CREATE TABLE IF NOT EXISTS recipes (
+    id INTEGER PRIMARY KEY  AUTOINCREMENT,
+    title TEXT,
+    web_address TEXT,
+    ingredients TEXT);"""
+    cursor.execute(create_statement)
+
 def get_params():
     ingredients = input("what ingredients shall we include? (comma separated)")
     recipe_type = input("What kind of recipe shall we look for:")
@@ -24,7 +38,13 @@ def main():
     params = get_params()
     loc = f"http://www.recipepuppy.com/api/?i={params[0]}&q={params[1]}"
     print(loc)
+    connection = sqlite3.connect("recipes.db")
+    cursor = connection.cursor()
+    setup_database(cursor)
     data = get_data(loc)
+    save_data(data, cursor)
+    connection.commit()
+    connection.close()
     display_data(data)
 
 
